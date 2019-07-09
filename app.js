365b00b5d9c94f3cc73d5711d0ee95f511373401
CPark <parkchaeah331@gmail.com>
2019-07-09 19:37:29 +0300

    architect web app

---
+++ app.js
@@ -0,0 +1,29 @@
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+  const temperatureDegree = document.querySelector(".temperature-degree");
+  const temperatureDescription = document.querySelector(".temperature-description");
+  const locationTimezone = document.querySelector(".location-timezone");
+
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition( position => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+      const api = `${proxy}https://api.darksky.net/forecast/6b801feb829ae8086c92401db081668a/${lat},${long}`;
+
+      fetch(api)
+       .then(response => {
+         return response.json();
+       })
+       .then(data => {
+         // get data of temperature and summary
+         const {temperature, summary} = data.currently;
+         temperatureDegree.textContent = temperature;
+         temperatureDescription.textContent = summary;
+         locationTimezone.textContent = data.timezone;
+       })
+    })
+  }
+})
